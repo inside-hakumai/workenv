3c27631d815871b10af44487c30fe9320c744358
Hakumai <inside.rice164@gmail.com>
2017-12-27 16:17:54 +0900

    Add alias of git log --graph

--- fish/config.fish
+++ fish/config.fish
@@ -34,6 +34,7 @@ alias o='open'
 alias e='emacs'
 alias eamcs='emacs'
 alias ls='ls -hGla --color=auto'
+alias gitg='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
 
 # other aliases
 alias rm='trash'
