efd1bbc812ba8362a35501e771539d1ed6d78a52
Hakumai <inside.rice164@gmail.com>
2017-12-28 13:39:07 +0900

    Replace . to source and complement missing -x of set

--- fish/config.fish
+++ fish/config.fish
@@ -85,8 +85,8 @@ end
 
 
 # activate command line powerline pronpt
-set fish_function_path $fish_function_path "(POWERLINE_ROOT)/bindings/fish"
-. $POWERLINE_ROOT/bindings/fish/powerline-setup.fish
+set -x fish_function_path $fish_function_path "(POWERLINE_ROOT)/bindings/fish"
+source $POWERLINE_ROOT/bindings/fish/powerline-setup.fish
 powerline-setup
 
 # make "rbenv shell"available
