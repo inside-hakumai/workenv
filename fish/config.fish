8481246d4dcb4669f8a2bfbcc61f8c965b61b32a
inside-hakumai <inside.rice164@gmail.com>
2019-08-04 01:42:13 +0900

    Introduce exit code view to fish right prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,48 @@
 # disable "Welcome to fish, the friendly interactive shell"
 set fish_greeting
 
-set PROMPT_ARROW 
+set PROMPT_R_ARROW   # PowerlineFontを導入することで表示できる右向きの三角形アイコン
+set PROMPT_L_ARROW   # PowerlineFontを導入することで表示できる左向きの三角形アイコン
 set PROMPT_BG_BLACK_FG_WHITE (set_color -b black; and set_color white)
 set PROMPT_BG_BLUE_FG_BLACK (set_color -b blue; and set_color black)
 set PROMPT_BG_NORMAL_FG_BLUE (set_color -b normal; and set_color blue)
 set PROMPT_COLOR_NORMAL (set_color -b normal; and set_color normal)
+set pf_b (set_color black)
+set pb_b (set_color -b black)
+set pf_g (set_color green)
+set pf_n (set_color normal)
+set pf_r (set_color red)
+set pb_r (set_color -b red)
+set pf_y (set_color yellow)
+set pf_by (set_color -o yellow)
+set pb_y (set_color -b yellow)
 set fish_prompt_pwd_dir_length 2
 
+set CHECK_ICON 
+set EXIT_ICON "↵"
+
+
+function store_exit_code --on-event fish_prompt
+  set -g RETVAL "$status"
+end
+
+
+function exit_code_prompt
+  if [ $RETVAL -eq 0 ]
+    printf "$pf_b$PROMPT_L_ARROW$pf_g$pb_b %s  $pf_n" $CHECK_ICON
+  else
+    printf "$pf_r$PROMPT_L_ARROW$pf_by$pb_r %s %s $pf_n" $RETVAL $EXIT_ICON
+  end
+end
+
+
+function fish_right_prompt --description 'Write out the right prompt'
+    printf "%s" (exit_code_prompt)
+end
+
+
 function fish_prompt --description 'Write out the prompt'
-    printf "$PROMPT_BG_BLACK_FG_WHITE %s $PROMPT_BG_BLUE_FG_BLACK$PROMPT_ARROW %s $PROMPT_BG_NORMAL_FG_BLUE$PROMPT_ARROW$PROMPT_COLOR_NORMAL " (whoami) (prompt_pwd)
+    printf "$PROMPT_BG_BLACK_FG_WHITE %s $PROMPT_BG_BLUE_FG_BLACK$PROMPT_R_ARROW %s $PROMPT_BG_NORMAL_FG_BLUE$PROMPT_R_ARROW$PROMPT_COLOR_NORMAL " (whoami) (prompt_pwd)
 end
 
 # detect using OS
@@ -57,7 +90,7 @@ alias gitg='git log --graph --full-history --all --color --pretty=format:"%x1b[3
 
 if test "$TARGET_OS" = "MacOS"
     alias ls='ls -hGla'
-else if test "$TARGET_OS" = "Linux" 
+else if test "$TARGET_OS" = "Linux"
     alias ls='ls -hGla --color=auto'
 end
 
@@ -75,7 +108,7 @@ set -x NODE_PATH /usr/local/lib/node_modules
 
 # JAVA_HOME configuration
 if [ -f /usr/libexec/java_home ]
-    set jhome (eval /usr/libexec/java_home -v 1.8 ^ /dev/null)
+    set jhome (eval /usr/libexec/java_home -v 11 ^ /dev/null)
     if [ $status -eq 0 ]
         set -x JAVA_HOME $jhome
     else if [ $status -eq 1 ]
@@ -96,9 +129,9 @@ end
 # pyenv configuration
 if [ -e $HOME/.pyenv ]
     set -x PYENV_ROOT "$HOME/.pyenv"
-    set -x PATH "$PYENV_ROOT/bin" $PATH
+    # set -x PATH "$PYENV_ROOT/bin" $PATH
     if builtin command -v pyenv > /dev/null
-        . (pyenv init - | psub)
+        status --is-interactive; and source (pyenv init -| psub)
     else
         echo "[Notice] pyenv is not installed." >&2
     end
