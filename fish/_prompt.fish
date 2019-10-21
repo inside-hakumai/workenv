7dd7fff537db25dc3c15845303a67edc29d071d7
inside-hakumai <inside.rice164@gmail.com>
2019-10-22 01:31:05 +0900

    Simplify descriptions that change prompt color

--- fish/_prompt.fish
+++ fish/_prompt.fish
@@ -1,5 +1,6 @@
-set PROMPT_R_ARROW   # PowerlineFontを導入することで表示できる右向きの三角形アイコン
-set PROMPT_L_ARROW   # PowerlineFontを導入することで表示できる左向きの三角形アイコン
+set PROMPT_R_TRIANGLE   # PowerlineFontを導入することで表示できる右向きの三角形アイコン
+set PROMPT_L_TRIANGLE   # PowerlineFontを導入することで表示できる左向きの三角形アイコン
+set PROMPT_R_ARROW   # PowerlineFontを導入することで表示できる「>」のようなアイコン
 set pf_b (set_color black)
 set pb_b (set_color -b black)
 set pf_bl (set_color blue)
@@ -22,9 +23,39 @@ set EXIT_ICON "↵"
 
 function exit_code_prompt
   if [ $RETVAL -eq 0 ]
-    printf "$pf_b$PROMPT_L_ARROW$pf_g$pb_b %s  $pf_n" $CHECK_ICON
+    printf "$pf_b$PROMPT_L_TRIANGLE$pf_g$pb_b %s  $pf_n" $CHECK_ICON
   else
-    printf "$pf_r$PROMPT_L_ARROW$pf_by$pb_r %s %s $pf_n" $RETVAL $EXIT_ICON
+    printf "$pf_r$PROMPT_L_TRIANGLE$pf_by$pb_r %s %s $pf_n" $RETVAL $EXIT_ICON
+  end
+end
+
+function python_virtual_env_prompt
+end
+
+function prompt_border
+  set next_bg_color $argv[1]
+  if set -q current_bg_color
+    echo " "
+    set_color -b $next_bg_color
+    set_color $current_bg_color
+    echo $PROMPT_R_TRIANGLE
+  else
+    set_color -b $next_bg_color
+  end
+
+  echo " "
+
+  switch $next_bg_color
+    case "black"
+      set_color white
+    case "blue"
+      set_color black
+  end
+
+  if [ $next_bg_color = "normal" ]
+    set -e current_bg_color
+  else
+    set -g current_bg_color $next_bg_color
   end
 end
 
@@ -35,7 +66,7 @@ end
 
 # fishのプロンプトの装飾
 function fish_prompt --description 'Write out the prompt'
-  printf "$pf_w$pb_b %s $pf_b$pb_bl$PROMPT_R_ARROW %s $pb_n$pf_bl$PROMPT_R_ARROW$pf_n " (whoami) (prompt_pwd)
+  printf "%s%s%s%s%s" (prompt_border black) (whoami) (prompt_border blue) (prompt_pwd) (prompt_border normal)
 end
 
 # fishのプロンプト（右側）の装飾
