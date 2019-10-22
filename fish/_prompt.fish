d010c9583a13b9c98cf36ef2f7f39ae72678e134
inside-hakumai <inside.rice164@gmail.com>
2019-10-23 01:48:39 +0900

    Remove unused variables

--- fish/_prompt.fish
+++ fish/_prompt.fish
@@ -3,22 +3,6 @@ set PROMPT_L_TRIANGLE   # PowerlineFontを導入することで表示でき�
 set PROMPT_R_ARROW   # PowerlineFontを導入することで表示できる「>」のようなアイコン
 set PYTHON_ICON   # PowerlineFontを導入することで表示できるPythonのロゴマーク
 
-# TODO: あんまり使われてないので消す
-set pf_b (set_color black)
-set pb_b (set_color -b black)
-set pf_bl (set_color blue)
-set pb_bl (set_color -b blue)
-set pf_w (set_color white)
-set pb_w (set_color -b white)
-set pf_g (set_color green)
-set pf_r (set_color red)
-set pb_r (set_color -b red)
-set pf_y (set_color yellow)
-set pf_by (set_color -o yellow)
-set pb_y (set_color -b yellow)
-set pf_n (set_color normal)
-set pb_n (set_color -b normal)
-
 set fish_prompt_pwd_dir_length 2
 
 set CHECK_ICON 
@@ -29,9 +13,9 @@ set prompt_fragments "whoami/black/white" "python_virtual_env_prompt/yellow/blac
 
 function exit_code_prompt
   if [ $RETVAL -eq 0 ]
-    printf "$pf_b$PROMPT_L_TRIANGLE$pf_g$pb_b %s  $pf_n" $CHECK_ICON
+    echo -n -s (set_color black) "$PROMPT_L_TRIANGLE" (set_color green) (set_color -b black) " $CHECK_ICON  " (set_color normal)
   else
-    printf "$pf_r$PROMPT_L_TRIANGLE$pf_by$pb_r %s %s $pf_n" $RETVAL $EXIT_ICON
+    echo -n -s (set_color red) "$PROMPT_L_TRIANGLE" (set_color -o yellow) (set_color -b red) " $RETVAL $EXIT_ICON " (set_color normal)
   end
 end
 
