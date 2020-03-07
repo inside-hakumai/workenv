cf88038bcae7d99f4401bcdb00e1fbab32ff6b9a
inside-hakumai <inside.rice164@gmail.com>
2020-03-08 01:44:58 +0900

    Add a process in install.sh to install libraries from Homebrew

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ if ! (hash brew 2>/dev/null) ; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
+brew install git anyenv tmux zsh fish
+
 
 
 #if [ ! -e ~/workspace/Env ] ; then
