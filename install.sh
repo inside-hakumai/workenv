984be1e7783410b12f503d7582cd015a1b219f72
inside-hakumai <inside.rice164@gmail.com>
2020-03-08 01:16:21 +0900

    Add a process in install.sh to install homebrew

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+if ! (hash brew 2>/dev/null) ; then
+  # TODO: 途中でエンターキーの押下とパスワード入力を要求されるので自動化したい
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+
+
+#if [ ! -e ~/workspace/Env ] ; then
+#  git clone https://github.com/inside-hakumai/Env.git ~/workspace/Env
+#  cd ~/workspace/Env
+#else
+#  cd ~/workspace/Env
+#  git chekcout master
+#  git pull origin master
+#fi
