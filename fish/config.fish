9e6afb72ea48f638d2708a3aa316d8561af6f657
inside-hakumai <inside.rice164@gmail.com>
2020-05-04 18:08:04 +0900

    Add an alias

--- fish/config.fish
+++ fish/config.fish
@@ -50,6 +50,7 @@ end
 alias o='open'
 alias e='emacs'
 alias eamcs='emacs'
+alias restart='exec $SHELL -l'
 alias gitg='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
 
 if test "$TARGET_OS" = "MacOS"
