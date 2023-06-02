6e352207bda9dfb7aff22534c0f97437009adec1
inside-hakumai <inside.rice164@gmail.com>
2023-06-02 21:49:32 +0900

    .gitconfigを設定するためのスクリプトを追加

---
+++ git_assets/setup_gitconfig.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eu
+
+git config --global user.name "inside-hakumai"
+git config --global user.email "inside.rice164@gmail.com"
+git config --global core.excludesfile "~/.gitignore_global"
